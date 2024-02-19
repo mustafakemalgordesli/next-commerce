5a4630331ec851e1994bc3751f5acfcdbc567f2e
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2024-02-19 15:32:27 +0300

    Add google auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import prisma from '@/lib/prisma';
+import { cookies } from 'next/headers';
+import { createJwtToken } from '@/lib/utils';
+
+const EXP_TIME = 24 * 60 * 60 * 30;
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -9,6 +14,49 @@ export const authOptions: NextAuthOptions = {
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET as string,
+
+    callbacks: {
+        async signIn({ account, profile }) {
+            try {
+                const cookieStore = cookies();
+                let user = await prisma.user.findUnique({
+                    where: {
+                        email: profile?.email,
+                    },
+                });
+                if (!user) {
+                    const newUser = {
+                        //@ts-ignore
+                        firstname: profile?.given_name,
+                        //@ts-ignore
+                        lastname: profile?.family_name,
+                        email: profile?.email || '',
+                        password: '--------------------',
+                    };
+                    if (newUser.email == undefined || newUser.email == '')
+                        return false;
+                    user = await prisma.user.create({ data: newUser });
+                }
+
+                const token = await createJwtToken({
+                    id: user.id,
+                    email: user.email,
+                });
+                cookieStore.set({
+                    name: 'token',
+                    value: token,
+                    httpOnly: true,
+                    path: '/',
+                    secure: process.env.NODE_ENV !== 'development',
+                    maxAge: EXP_TIME,
+                });
+
+                return true;
+            } catch (error) {
+                return false;
+            }
+        },
+    },
 };
 
 const handler = NextAuth(authOptions);
